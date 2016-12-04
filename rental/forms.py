22c25204bf2f71440ed6b9779cbe603da313a20d
choisungwook <kgg1959@naver.com>
2016-12-05 01:29:26 +0900

    Update forms.py

--- rental/forms.py
+++ rental/forms.py
@@ -5,8 +5,17 @@ from address.models import Address, Sido, Sigungu
 from django.forms import ModelChoiceField
 from shop.models import Store
 from address.models import Sigungu, Sido, Address
+from rental.models import Rentalinventory
+
+def queryset():
+    rental = Rentalinventory.objects.all().values_list('store')
+    store = Store.objects.filter(id__in=rental).values_list('address')
+    address = Address.objects.filter(id__in=store).values_list('Sigungu')
+    sigungu = Sigungu.objects.filter(id__in=address).values_list('sido')
+
+    return Sido.objects.filter(id__in=sigungu)
 
 class SigunguForm(forms.Form):
-    sido = forms.ModelChoiceField(label="시도", widget=forms.Select(attrs={'class' : 'form-control'}), queryset=Sido.objects.filter(id__in=Sigungu.objects.filter(id__in= \
-                                                    Address.objects.filter(id__in=Store.objects.values_list('address').distinct()).values_list('Sigungu').distinct()).values_list('sido_id')))
+    sido = forms.ModelChoiceField(label="시도", widget=forms.Select(attrs={'class' : 'form-control'}),
+    queryset= queryset())
     sigungu = forms.CharField(label="시군구", widget=forms.Select(attrs={'class' : 'form-control'}))
